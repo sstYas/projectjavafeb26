364385083602a2a47db0b1dc835c0b23c93c1c3c
sstYas <mohamadss8525@gmail.com>
2026-02-22 12:49:49 +0530

    done

--- src/org/zoho/SampleGitClass.java
+++ src/org/zoho/SampleGitClass.java
@@ -2,4 +2,22 @@ package org.zoho;
 
 public class SampleGitClass {
 
+	
+	public static  void gitData1() {
+		System.out.println("Git data 1 ");
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		gitData1();
+		
+		int a1 = 4443;
+		System.out.println(a1);
+		
+		
+		
+	}
+	
+	
 }
